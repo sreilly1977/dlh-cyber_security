0cf59ca45caa5a9b69a146884e2fe1072d03e06d
sreilly1977 <steve@devnull.lu>
2026-05-18 14:04:59 +0200

    Cleaned up some scripts

--- cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
@@ -1,2 +1,2 @@
 !/bin/bash
-hashcat -m 0 -a 0 $1 /usr/share/wordlists/rockyou.txt --show > 7-password.txt
+hashcat -m 0 -a 0 $1 /usr/share/wordlists/rockyou.txt --show cut -d':' -f2 > 7-password.txt
--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -m 0 -a 1 $1 wordlist1.txt wordlist2.txt --show > 9-password.txt 
+hashcat -m 0 -a 1 $1 wordlist1.txt wordlist2.txt --show cut -d':' -f2 > 9-password.txt 
