1f7f620e2b199845a764c7349fa082dfb009e8dc
sreilly1977 <steve@devnull.lu>
2026-05-26 08:07:25 +0200

    Another try at 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant|Admin|Tech/{i=index($0,": "); f=substr($0,1,i-1); v=substr($0,i+2); if(f~/Street/)v=v" "; if(f~/Ext/)f=f":"; print f","v}' > $1.csv
+whois $1 | awk '/Registrant|Admin|Tech/{split($0,a,":"); gsub(/^[ ,]+|[ ,]+$/, "", a[1]); v=a[2]; sub(/^[ ,]+/, "", v); if(a[1]~/Street/)v=v" "; if(a[1]~/Ext/)a[1]=a[1]":"; print a[1]","v}' > $1.csv
