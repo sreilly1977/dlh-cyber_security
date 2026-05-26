dad79b8cc9aea32c541a8fd09e15914f406d9f14
sreilly1977 <steve@devnull.lu>
2026-05-26 10:05:01 +0200

    SMTP for the win

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-if ! grep -i "smtpd_tls_security_level = may" /etc/postfix/main.cf ; then echo "STARTTLS not configured" ; fi
+grep "^smtpd_tls_security_level" /etc/postfix/main.cf | echo "STARTTLS not configured"
