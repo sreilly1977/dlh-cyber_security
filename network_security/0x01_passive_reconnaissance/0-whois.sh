706f3589fd04cf8589df5a690d148967f8ddda74
sreilly1977 <steve@devnull.lu>
2026-05-26 08:21:55 +0200

    Nearly there, was missing a space 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant|Admin|Tech/ && !/information on how to contact/{split($0,a,":"); gsub(/^[ ,]+|[ ,]+$/, "", a[1]); v=a[2]; sub(/^[ ,]+/, "", v); if(a[1]~/Registry.*ID/)next; if(a[1]~/Street/)v=v" "; if(a[1]~/Ext/)a[1]=a[1]":"; print a[1]","v}' > $1.csv
+whois $1 | awk '/Registrant|Admin|Tech/ && !/information on how to contact/{split($0,a,":"); gsub(/^[ ,]+|[ ,]+$/, "", a[1]); v=a[2]; sub(/^[ ,]+/, "", v); if(a[1]~/Registry.*ID/)next; if(a[1]~/Street/)v=v" "; if(a[1]~/Ext/)a[1]=a[1]":"; print a[1]", "v}' > $1.csv
