b112ad0ccb50d34befd9043a5ceadc8446c4e369
Stephen Reilly <stephen.p.reilly@gmail.com>
2026-06-08 18:32:10 +0200

    Second attempt at 2-flag.txt

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c "import base64; data=base64.b64decode('${1#\{xor\}}'); key= 0x5F; print(bytes([b ^ key for b in data]).decode())"
+python3 -c "import base64,sys;b=base64.b64decode(sys.argv[1]);print(''.join(chr(c^0x5F) for c in b))" "${1#\{xor\}}"
