f1c66bd37e884cb6f85a3eca922fbbc5ca97f57a
Stephen Reilly <stephen.p.reilly@gmail.com>
2026-06-08 16:07:19 +0200

    Third attempt at WebSphere XOR decoder

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c "import base64; data=base64.b64decode('${1#\{xor\}}'); key= 0x5F; print(bytes([b ^ key for b in data]).decode())""
+python3 -c "import base64; data=base64.b64decode('${1#\{xor\}}'); key= 0x5F; print(bytes([b ^ key for b in data]).decode())"
